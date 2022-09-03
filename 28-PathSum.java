9c4dc622d4e2b1d889a05761abcc99e05b2b8ff1
Sahisnu <43699421+ssahisnu@users.noreply.github.com>
2022-09-03 11:44:16 +0530

    Create 28-PathSum.java

---
+++ 28-PathSum.java
@@ -0,0 +1,14 @@
+class Solution {
+    //TC - O(N)
+    //SC - O(H)/O(N)
+    public boolean hasPathSum(TreeNode root, int sum){
+        if(root == null) return false;
+    
+        if(root.left == null && root.right == null 
+           && sum - root.val == 0) return true;
+    
+        return hasPathSum(root.left, sum - root.val) || 
+            hasPathSum(root.right, sum - root.val);
+    }
+    
+}
